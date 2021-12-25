8a229e7549451f119408a4c517f630720e3c20a7
lwasinam <lwasinamdilli@gmail.com>
2021-12-26 00:17:00 +0100

    fixing state management

--- lib/pages/WelcomeScreen/WelcomeScreen.dart
+++ lib/pages/WelcomeScreen/WelcomeScreen.dart
@@ -1,5 +1,9 @@
+import 'dart:typed_data';
+
 import 'package:ebook_jotter/import/imports.dart';
+import 'package:ebook_jotter/models/Model.dart';
 import 'package:ebook_jotter/providers/DataProvider.dart';
+import 'package:native_pdf_view/native_pdf_view.dart';
 
 
 
@@ -41,7 +45,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   )]
                 ),
                 child: IconButton(onPressed: () async {
-               DataProvider().pickBook(context);
+               pickBook(context);
                 }, icon: Icon(Icons.add))),
             )
           ],
@@ -49,4 +53,77 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       ),
     );
   }
+
+
+
+
+
+  pickBook(context) async {
+    
+   
+
+     FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+      if (result != null) {
+        //get pdf path
+          String? file = result.files.single.path;
+
+          // convert first page to image
+          final document = await PdfDocument.openFile(file!);
+          final page = await document.getPage(1);
+          final coverImage = await page.render(width: page.width, height: page.height);
+          await page.close();
+         
+         // convert image file in bytes to string
+        Uint8List? coverImageString = coverImage?.bytes;
+
+        BookNameDialog(file,coverImageString, context );
+       
+
+          
+ } 
+ else {
+                         // User canceled the picker
+      }
+  }
+
+
+  //dialog to write book name
+  Future BookNameDialog(file, coverImageString, context) => showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text("Write book name"),
+    
+      content: TextField(
+        controller: textController ,
+        autofocus: true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12))
+        ),
+      ),
+        actions: [
+      
+        TextButton(onPressed: ()  {
+          Navigator.of(context).pop();
+          final model = Provider.of<pdfModels>(context).DataProvider().
+       
+              addToHive(pdfModels(filepath:file ,imageFile: coverImageString ,pageNumber: 1, notePath: '', bookName:textController?.text ));
+        }, child: Text("Ok"))
+      ],
+    )
+
+
+  );
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- lib/providers/DataProvider.dart
+++ lib/providers/DataProvider.dart
@@ -9,69 +9,27 @@ class DataProvider extends ChangeNotifier {
   final TextEditingController? textController = new TextEditingController();
 
   static Box<pdfModels>  openHiveBox() => Hive.box("BooksDatabase");
+  
+  pdfModels _pdfodel = pdfModels(bookName: '', filepath: '', notePath: '', pageNumber: 0, imageFile: Uint8List.fromList([00]));
+  
+  pdfModels get pdfmodel => _pdfodel;
 
-  //dialog to write book name
-  Future BookNameDialog(file, coverImageString, context) => showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: Text("Write book name"),
-    
-      content: TextField(
-        controller: textController ,
-        autofocus: true,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12))
-        ),
-      ),
-        actions: [
-      
-        TextButton(onPressed: ()  {
-          Navigator.of(context).pop();
-       
-              addToHive(pdfModels(filepath:file ,imageFile: coverImageString ,pageNumber: 1, notePath: '', bookName:textController?.text ));
-        }, child: Text("Ok"))
-      ],
-    )
+  set pdfmodel (pdfModels newModel){
+    _pdfodel = newModel;
+    _addToHive(_pdfodel);
 
 
-  );
+  }
+
 
   //adds instance of class to hive database
-  addToHive(pdfModels pdfModel) async {
+   _addToHive(pdfModels pdfmodel) async {
     Box  box = DataProvider.openHiveBox();// adds to database
-    box.add(pdfModel);
+    box.add(pdfmodel);
     notifyListeners();
    box.isEmpty ? print ("true") : print ("false");
   }
 
-  pickBook(context) async {
-    
-   
-
-     FilePickerResult? result = await FilePicker.platform.pickFiles();
-
-      if (result != null) {
-        //get pdf path
-          String? file = result.files.single.path;
-
-          // convert first page to image
-          final document = await PdfDocument.openFile(file!);
-          final page = await document.getPage(1);
-          final coverImage = await page.render(width: page.width, height: page.height);
-          await page.close();
-         
-         // convert image file in bytes to string
-        Uint8List? coverImageString = coverImage?.bytes;
-
-        BookNameDialog(file,coverImageString, context );
-       
-
-          
- } 
- else {
-                         // User canceled the picker
-                    }
-  }
 
 //Todo implement pick book function which gets filepath and pdf cover pic
 //reading efficiently from hive db
